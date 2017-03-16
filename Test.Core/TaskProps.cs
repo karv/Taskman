ca7f0ec605611d848f6261ef8f8bc68a397cb76c
Edgar Carballo <karvayoedgar@gmail.com>
2017-03-16 14:58:33 -0600

    Test (passed)

--- Test.Core/TaskProps.cs
+++ Test.Core/TaskProps.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using Taskman;
 
 namespace Test
@@ -13,6 +14,29 @@ namespace Test
 			var task = col.AddNew ();
 			Assert.IsNotNull (task);
 			Assert.NotNull (task.ActivityTime);
+		}
+
+		static void switchAndTest (Task task, TaskStatus newStatus)
+		{
+			task.Status = newStatus;
+			Assert.AreEqual (newStatus, task.Status);
+		}
+
+		[Test]
+		public void StatusSetter ()
+		{
+			var col = new TaskCollection ();
+			var task = col.AddNew ();
+
+			switchAndTest (task, TaskStatus.Active);
+			switchAndTest (task, TaskStatus.Completed);
+			var acc0 = task.TotalActivityTime;
+			Assert.Greater (acc0, TimeSpan.Zero);
+
+			// Backward
+			switchAndTest (task, TaskStatus.Active);
+			switchAndTest (task, TaskStatus.Inactive);
+			Assert.Greater (task.TotalActivityTime, acc0);
 		}
 	}
 }
