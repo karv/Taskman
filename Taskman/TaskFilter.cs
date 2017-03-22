6083bd52880fd15e9e6f7421cedcfc0ae0efb072
Edgar Carballo <karvayoedgar@gmail.com>
2017-03-22 16:30:21 -0600

    Apply the filter to all its childs

--- Taskman/TaskFilter.cs
+++ Taskman/TaskFilter.cs
@@ -42,10 +42,28 @@ namespace Taskman.Gui
 		{
 			var taskId = (int)model.GetValue (iter, 0);
 			var task = Tasks.GetById<Task> (taskId);
+			return ApplyFilter (task);
+		}
+
+		public bool ApplyFilter (Task task)
+		{
+			// TODO
+			if (task.GetSubtasks ().Any (ApplyFilter))
+				return true;
+			// Supose task has ot visible childs
 			var filter = CatRules ();
 			return filter.All (z => task.HasCategory (z.Item1) == z.Item2) &&
 			(ShowCompleted || task.Status != TaskStatus.Completed) &&
 			(ShowInactive || task.Status == TaskStatus.Active);
+			/*
+			// If exist a rule z such that for any subtask whose catValue does not coincide with z, return false
+			if (filter.Any (z => !task.HasOrExistSubtaskWithCategory (z.Item1, z.Item2)))
+				return false;
+			var visSubTask = task.GetSubtasks ().Where (ApplyFilter);
+			return 
+			(ShowCompleted || task.Status != TaskStatus.Completed) &&
+			(ShowInactive || task.Status == TaskStatus.Active);
+			*/
 		}
 
 		/// <param name="tasks">Task collection</param>
