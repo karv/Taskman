fc9985cde8a63b13964895521ba3545385788683
Edgar Carballo <karvayoedgar@gmail.com>
2017-03-25 16:31:08 -0600

    Add tests and pass

---
+++ Test.Core/Import.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using Taskman;
+using Taskman.Import;
+
+namespace Test
+{
+	[TestFixture]
+	public class Import
+	{
+		public TaskCollection BaseColl;
+		public TaskCollection ImportColl;
+
+		[SetUp]
+		public void Setup ()
+		{
+			BaseColl = new TaskCollection ();
+			ImportColl = new TaskCollection ();
+		}
+
+		[Test]
+		public void SimpleImport ()
+		{
+			var baseCat = BaseColl.AddCategory ();
+			baseCat.Name = "base";
+			var importCat = ImportColl.AddCategory ();
+			importCat.Name = "import";
+			BaseColl.ImportCategory (ImportColl);
+
+			Assert.AreEqual (2, BaseColl.EnumerateCategories ().Count ());
+		}
+
+		[Test]
+		public void MergeImport ()
+		{
+			var baseCat = BaseColl.AddCategory ();
+			baseCat.Name = "base";
+			var importCat = ImportColl.AddCategory ();
+			importCat.Name = "base";
+			BaseColl.ImportCategory (ImportColl);
+
+			Assert.AreEqual (1, BaseColl.EnumerateCategories ().Count ());
+		}
+	}
+}
