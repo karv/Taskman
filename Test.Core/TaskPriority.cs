b064f278c2c51096158c38437fcba1884a8f70f2
Edgar Carballo <karvayoedgar@gmail.com>
2017-03-29 17:31:37 -0600

    Pass test

---
+++ Test.Core/TaskPriority.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using Taskman;
+
+namespace Test
+{
+	[TestFixture]
+	public class TaskPriority
+	{
+		public TaskCollection Collection;
+
+		[SetUp]
+		public void Setup ()
+		{
+			Collection = new TaskCollection ();
+		}
+
+		public void GoodCalculation ()
+		{
+			var root = Collection.AddNew ();
+			var sub0 = root.CreateSubtask ();
+			var sub1 = root.CreateSubtask ();
+			sub0.SelfPriority = 3;
+			sub1.SelfPriority = 2;
+
+			Assert.AreEqual (3, root.RecursivePriority);
+		}
+	}
+}
